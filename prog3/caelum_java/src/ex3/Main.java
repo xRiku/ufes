22bb76ac84357fa8b0668199c0593cdc137e5cbf
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2019-11-10 13:32:44 -0300

    Exercicio 3

---
+++ prog3/caelum_java/src/ex3/Main.java
@@ -0,0 +1,28 @@
+/* Programa 3 
+ * Classe: Casa
+ * Atributos: cor, porta1, porta2, porta3 
+ * Método: void pinta(String s), int quantasPortasEstaoAbertas() 
+ * Crie uma casa e pinte-a. Crie três portas e coloque-as na casa; 
+ * Abra e feche as mesmas como desejar. 
+ * Utilize o método quantasPortasEstaoAbertas para imprimir o número de portas abertas.
+ */
+
+
+class Main{
+    public static void main(String[] args){
+        int quantidade;
+        Casa casa = new Casa();
+        casa.porta1 = new Porta();
+        casa.porta2 = new Porta();
+        casa.porta3 = new Porta();
+        System.out.println(casa.cor);
+        casa.pinta("Branca");
+        System.out.println(casa.cor);
+        quantidade = casa.quantasPortasEstaoAbertas();
+        System.out.printf("Quantidade: %d\n", quantidade);
+        casa.porta1.abre();
+        casa.porta3.abre();
+        quantidade = casa.quantasPortasEstaoAbertas();
+        System.out.printf("Quantidade: %d\n", quantidade);
+    }
+}
