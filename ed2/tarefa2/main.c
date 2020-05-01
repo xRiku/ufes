f633c416cdb2963e70cc6e71b23b465a072cc2f9
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-04-30 23:42:19 -0300

    arquivos placeholders criados com make funcional

---
+++ ed2/tarefa2/main.c
@@ -0,0 +1,7 @@
+#include "union_find.h"
+
+int main() {
+  int N, Q;
+  scanf("%d %d", &N, &Q);
+  printf("%d %d\n", N, Q);
+}
---
+++ ed2/tarefa2/union_find.h
@@ -0,0 +1,8 @@
+#ifndef _UNIONFIND_
+#define _UNIONFIND_
+#include <stdio.h>
+#include <stdio.h>
+
+
+
+#endif
