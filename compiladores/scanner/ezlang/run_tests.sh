faa9eaefcdb31f022346bf05e747d7f34656f242
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2021-06-29 16:24:38 -0300

    Script corrigido

--- compiladores/scanner/ezlang/run_tests.sh
+++ compiladores/scanner/ezlang/run_tests.sh
@@ -2,9 +2,9 @@
 EXE=./lab01
 IN=in
 OUT=out01
-for infile in ‘ls $IN/*.ezl‘; do
+for infile in $IN/*.ezl; do
     base=$(basename $infile)
     outfile=$OUT/${base/.ezl/.out}
     echo $infile
-    $EXE < $infile | diff -w $outfile -
+    $EXE < $infile | code --diff $outfile -
 done
